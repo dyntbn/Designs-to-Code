57815f55e22f6c3eb92234cc6c26233168883692
dyntbn <dyntbn@gmail.com>
2022-03-31 17:22:36 -0700

    Completed interactive rating component. * added space select controls for buttons. * added gradient background for components. * added media queries for responsive design. * added animated fade in on submit to card. * added animated button feedback.

--- challenges/interactive-rating-component/main.js
+++ challenges/interactive-rating-component/main.js
@@ -2,17 +2,46 @@ document.body.querySelector('.card__btn').addEventListener('click', () => {
 	var ratingValue = document.body.querySelectorAll('.card__radio:checked');
 
 	// Prevent scene change if document data isn't correct or present
-	if (
-		ratingValue == null ||
-		ratingValue == undefined ||
-		ratingValue.length == 0 ||
-		ratingValue[0].value > 5 ||
-		ratingValue[0].value < 0
-	)
+	if (ratingValue == null || ratingValue == undefined || ratingValue.length == 0) {
+		showErrorFeedback('No Rating Selected');
 		return;
+	}
+	if (ratingValue[0].value > 5 || ratingValue[0].value < 0) {
+		showErrorFeedback('Rating Out of Bounds');
+		return;
+	}
 
-	// Hide rating card and display submitted rated card.
+	// Hide rating card then display submitted rated card.
 	document.body.querySelector('.card__feedback span').textContent = ratingValue[0].value;
-	document.body.querySelector('.card--inactive').classList.remove('card--inactive');
+	let ratedScene = document.body.querySelector('.card--inactive');
+	ratedScene.classList.remove('card--inactive');
+	ratedScene.classList.add('card--fade-in');
 	document.body.querySelector('.card').classList.add('card--inactive');
 });
+
+// Handle SPACEBAR as CLICK when using TAB-based selection
+document.body.querySelectorAll('.card__label').forEach((element) => {
+	element.addEventListener('keyup', (event) => {
+		if (event.which == '32') {
+			event.target.click(); // CLICK current selection
+		}
+	});
+});
+
+function showErrorFeedback(message, duration = 2000) {
+	// Present error to user
+	let submitElement = document.querySelector('.card__btn');
+	let isFocus = document.activeElement;
+	submitElement.disabled = true;
+	submitElement.classList.add('card__btn--shake');
+	submitElement.textContent = message;
+
+	// Enable new submission
+	setTimeout(() => {
+		submitElement.disabled = false;
+		submitElement.textContent = 'Submit';
+		submitElement.classList.remove('card__btn--shake');
+		if (isFocus) submitElement.focus();
+	}, duration);
+	return;
+}
